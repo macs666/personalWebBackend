ad43299d417c8088386037ff7a2a8ea22b03aeb8
muhammed azhar <macs@muhammeds-MacBook-Pro.local>
2018-12-25 14:01:24 +0530

    added message request model

--- server/boot/script.js
+++ server/boot/script.js
@@ -1,11 +1,17 @@
 module.exports = function(app) {
 
   app.post('/submitMessage', function(req, res) {
-    if(req.body.name != undefined || req.body.email != undefined || req.body.message != undefined) {
+    if(req.body.name != undefined || req.body.email != undefined || req.body.message != undefined || req.body.subject) {
       var name = req.body.name
     var email = req.body.email
     var message = req.body.message
-      var responseObj = { message: name+" with email "+email+" sent "+message}
+    var subject = req.body.subject
+      var responseObj = { 
+        name : name,
+        email : email,
+        subject : subject,
+        message: message
+      }
       res.send(responseObj)
     }
     else {
